5b6ec24dc01db955066d0b194048cfc30d600459
TarsilaNascMartins <tarsilawho@gmail.com>
2022-03-06 22:22:26 -0300

    v2

--- src/main/kotlin/eletronico.kt
+++ src/main/kotlin/eletronico.kt
@@ -1,7 +1,5 @@
 data class eletronico constructor ( var pesoProduto : Double, var tipoP : String, var tempoBateria : Int  )
 {
-
-
     var quantidade = 0
     var produto = " " // ex: tablet, celular, etc.
     var acessorio = " " //ex: capinha, fones
@@ -27,7 +25,24 @@ data class eletronico constructor ( var pesoProduto : Double, var tipoP : String
 
 
 
+        fun totalBateria(valor : Int){
+            if (valor <= 5 ) {
+                println("Você tem menos de 5% de bateria. Provavelmente perca algumas funções :( ")
+            } else if (valor > 5) {
+                println("Sua bateria está com todas as funções operando normalmente.")
+            }
+        }
 
+        fun recebeLig (valor : Int) {
+
+            if (valor <= 5) {
+                println("Realmente você tem pouca bateria. Não poderá receber ligações neste instante.")
+            } else if (valor > 5) {
+                println ("Você tem uma ligação...")
+            }
+
+
+        }
 
 
 }
